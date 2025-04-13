7305f4621718ef047ce4c2e465a8f685abd363a7
Yi-99 <ylim.8299@gmail.com>
2025-04-13 12:48:44 -0600

    hotfix: decodeURI component

--- src/app/advisor/[name]/page.tsx
+++ src/app/advisor/[name]/page.tsx
@@ -14,8 +14,8 @@ type Params = Promise<{ name: string }>
 
 export default function AdvisorPage(props: { params: Params }) {
 	const params = use(props.params);
-	const name = params.name;
-	
+	const name = decodeURIComponent(params.name);
+
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 			<h1 className="text-3xl font-bold">Advisor: {name}</h1>
