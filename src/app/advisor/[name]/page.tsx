6a1a7c0e030cd9c7751c62610ce836d9a7a6eae6
Yi-99 <ylim.8299@gmail.com>
2025-04-13 20:31:49 -0600

    hotfix: build fixed

--- src/app/advisor/[name]/page.tsx
+++ src/app/advisor/[name]/page.tsx
@@ -1,9 +1,11 @@
+"use client";
 import Textarea from "@/components/Textarea";
-import { use } from 'react';
+import { use, useState } from 'react';
 
 type Params = Promise<{ name: string }>
 
 export default function AdvisorPage(props: { params: Params }) {
+	const [value, setValue] = useState('');
 	const params = use(props.params);
 	const name = decodeURIComponent(params.name);
 
@@ -11,7 +13,7 @@ export default function AdvisorPage(props: { params: Params }) {
 		<div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 			<h1 className="text-3xl font-bold">Advisor: {name}</h1>
 			<p className="text-lg">This is the advisor page for {name}.</p>
-			<Textarea id="chat" name="chat" shouldReset={false} isChat={true}/>
+			<Textarea id="chat" name="chat" value={value} setValue={setValue} shouldReset={false} isChat={true}/>
 		</div>
 	)
 }
